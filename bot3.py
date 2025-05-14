a64ea4566662440a7d11c89687ae65084f79bf84
LucasGoulart0110 <llucasgoulart@gmail.com>
2025-05-13 21:51:43 -0300

    bot3.py

--- bot3.py
+++ bot3.py
@@ -1,15 +1,14 @@
 import discord
 from discord.ext import commands
 from googletrans import Translator
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
-
 translator = Translator()
 
-# IDs dos canais
 origem_channel_id = 1364692365909950485
 destino_channel_id = 1370180575452729424
 
@@ -23,14 +22,12 @@ async def on_message(message):
         return
 
     if message.channel.id == origem_channel_id:
-        # Detectar idioma da mensagem
         detectado = translator.detect(message.content)
         idioma = detectado.lang
 
         destino_channel = client.get_channel(destino_channel_id)
 
         if destino_channel:
-            # Verifica o idioma da mensagem
             if idioma == 'pt':
                 traduzido = translator.translate(message.content, dest='en')
                 embed = discord.Embed(
@@ -38,7 +35,6 @@ async def on_message(message):
                     description=f"**🇧🇷 Original:**\n{message.content}\n\n**🇺🇸 Tradução:**\n{traduzido.text}",
                     color=discord.Color.blue()
                 )
-                embed.set_thumbnail(url=message.author.avatar.url)  # Correção aqui
             elif idioma == 'en':
                 traduzido = translator.translate(message.content, dest='pt')
                 embed = discord.Embed(
@@ -46,7 +42,6 @@ async def on_message(message):
                     description=f"**🇺🇸 Original:**\n{message.content}\n\n**🇧🇷 Tradução:**\n{traduzido.text}",
                     color=discord.Color.green()
                 )
-                embed.set_thumbnail(url=message.author.avatar.url)  # Correção aqui
             else:
                 traduzido = translator.translate(message.content, dest='en')
                 embed = discord.Embed(
@@ -54,10 +49,9 @@ async def on_message(message):
                     description=f"**🌎 Original:**\n{message.content}\n\n**🇺🇸 Tradução:**\n{traduzido.text}",
                     color=discord.Color.purple()
                 )
-                embed.set_thumbnail(url=message.author.avatar.url)  # Correção aqui
 
+            embed.set_thumbnail(url=message.author.avatar.url)
             await destino_channel.send(embed=embed)
 
-import os
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-bot.run(TOKEN)
+client.run(TOKEN)
